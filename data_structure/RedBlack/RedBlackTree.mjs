e4b0dce3a3e3e4987bea34a037cccd9f9ccc3cb2
dy7791 <dy7791@naver.com>
2025-02-11 15:52:15 +0900

    Red-Black 트리 추상자료형 구현 주석 추가

--- data_structure/RedBlack/RedBlackTree.mjs
+++ data_structure/RedBlack/RedBlackTree.mjs
@@ -20,7 +20,7 @@ import { RedBlack_BinaryTree, RED, BLACK } from "./RedBlack_BinaryTree.mjs";
  * getSibling(node): 형제노드 구함
  * handleRedSibling(node, sibling): 형제 노드가 빨간색일 때 균형 맞춰줌
  * isBlack(node): 검은색인지 체크
- * handleBlackSiblingWithAtLeastOneRedChild():
+ * handleBlackSiblingWithAtLeastOneRedChild(node, sibling):
  */
 
 class RedBlackTree {
@@ -39,24 +39,34 @@ class RedBlackTree {
     return null;
   }
 
+  // 1 3 5 7 9 형태, 5를 회전한다고 가정
   rotateLeft(node) {
+    // node는 회전할 노드
+    // 회전을 하게 되면 그 자리에는 다른 노드가 들어오게 됨 >> 부모노드가 자식노드를 대체한 노드로 연결해줘야 한다.
     let parent = node.getParent();
     let rightChild = node.getRightSubTree();
+
+    // node(5) 회전하기 전에 오른쪽 자식노드(7)의 왼쪽 자식노드(NIL)를 node(5)의 오른쪽 자식노드로 연결해준다
     node.setRightSubTree(rightChild.getLeftSubTree());
 
     if (rightChild.getLeftSubTree() !== null) {
+      // 부모노드로 회전할 노드(5)를 가리키게 한다.
       rightChild.getLeftSubTree().setParent(node);
     }
 
     rightChild.setLeftSubTree(node);
     node.setParent(rightChild);
 
+    // 회전한 노드(5)의 부모노드(3)는 자식노드로 여전히 회전한 노드(5)를 가리키고 있으므로 바꿔줘야함
     this.replaceParentsChild(parent, node, rightChild);
   }
 
+  // 9 7 5 3 1 형태, 5를 회전한다고 가정
   rotateRight(node) {
     let parent = node.getParent();
     let leftChild = node.getLeftSubTree();
+
+    // node(5) 회전하기 전에 왼쪽 자식노드(3)의 오른쪽 자식노드(NIL)를 node(5)의 왼쪽 자식노드로 연결해준다
     node.setLeftSubTree(leftChild.getRightSubTree());
 
     if (leftChild.getRightSubTree() !== null) {
